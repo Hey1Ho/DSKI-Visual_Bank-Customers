88c2da20c34e64b3828de0e4946529cf01d72615
Valentin <majonaise24@gmail.com>
2025-02-20 13:55:03 +0100

    a

--- data_customers/Aufg6-8.py
+++ data_customers/Aufg6-8.py
@@ -1,10 +1,16 @@
 import pandas as pd
+import os
 
 
-df = pd.read_csv("02\data_customers\data\customer_attrition_0.csv", delimiter="|")
+# df = pd.read_csv("02\data_customers\data\customer_attrition_0.csv", delimiter="|")
+csv_files = [f for f in os.listdir("./data_customers/data") if f.endswith('.csv')]
+df_list = [pd.read_csv(os.path.join("./data_customers/data", file), delimiter="|") for file in csv_files]
+df = pd.concat(df_list, ignore_index=True)
 print(df.shape)
 
-print(df.info())
+# print(df.info())
+
+print(f"Duplikate: {df.duplicated().sum()}")
 
 def get_amountOf_male_over_50_not_from_france(df):
     return df.loc[
@@ -23,5 +29,6 @@ def get_credit_score_from_greatest_balance(df):
         df["balance"] == df["balance"].max()
     ]["credit_score"].values[0]
 
-# print(get_average_age_by_gender(df, "Male"))
+# print(get_amountOf_male_over_50_not_from_france(df))
+# print(get_average_age_by_gender(df, "Female"))
 # print(get_credit_score_from_greatest_balance(df))
